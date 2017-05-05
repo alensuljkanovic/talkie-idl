b1e5fa56aa95b15879d241c92d23c7b04b4dbbb0
Alen Suljkanović <alienized91@gmail.com>
2017-05-05 19:00:23 +0200

    Add new type system

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,44 +2,21 @@
 Tests simple example.
 """
 import os
-import pytest
-from talkie.generator.generator import TalkieGenerator
 from talkie.lang.meta import get_metamodel
 from talkie.utils import get_root_path
 
 
-def test_simple1():
+def test_complex_types():
     metamodel = get_metamodel()
     path = os.path.join(get_root_path(), "tests", "examples",
-                        "simple_interface.tl")
-    model = metamodel.model_from_file(path)
-
-    generator = TalkieGenerator(model)
-    generator.generate()
-
-
-def test_simple2():
-    metamodel = get_metamodel()
-    path = os.path.join(get_root_path(), "tests", "examples",
-                        "simple_interface2.tl")
-    model = metamodel.model_from_file(path)
-
-    generator = TalkieGenerator(model)
-    generator.generate()
-
-
-def test_duplicates():
-    metamodel = get_metamodel()
-    path = os.path.join(get_root_path(), "tests", "examples",
-                        "duplicates.tl")
-    with pytest.raises(Exception):
-        metamodel.model_from_file(path)
+                        "complex_types.tl")
+    metamodel.model_from_file(path)
 
 
 def test_typhoon_api():
     metamodel = get_metamodel()
     path = os.path.join(get_root_path(), "tests", "examples",
                         "typhoon_example.tl")
-    print(path)
-    package = metamodel.model_from_file(path)
-    print("")
+
+    metamodel.model_from_file(path)
+
